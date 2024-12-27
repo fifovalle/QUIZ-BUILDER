eb184ccca314b0510b485c9a5042c96c2551fdf5
fifovalle <fifanaufal10@gmail.com>
2024-12-27 18:34:46 +0700

    Completed

--- utils/supabaseClient.ts
+++ utils/supabaseClient.ts
@@ -1,6 +1,12 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl: string = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseAnonKey: string = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error(
+    "Variabel lingkungan NEXT_PUBLIC_SUPABASE_URL dan NEXT_PUBLIC_SUPABASE_ANON_KEY harus diatur."
+  );
+}
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
